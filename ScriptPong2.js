05e95e4d1d523e2c7cf3349450a72873782f8917
Pablo Acland <pablo.acland@estudiantes.utec.edu.uy>
2022-06-30 19:51:24 -0300

    Se realizo lo del segundo video

--- ScriptPong2.js
+++ ScriptPong2.js
@@ -22,6 +22,34 @@
 })();
 
 (function(){
+    //objeto Barra
+    self.Bar = function(x,y,width,height,board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+        this.speed = 25;
+    },
+
+    //prototipo de Barra
+    self.Bar.prototype ={
+        //Baja la Barra en el eje y
+        down: function(){
+            this.y += this.speed;
+        },
+        //Sube la Barra en el eje y
+        up: function(){
+            this.y -= this.speed;
+        }
+    }
+
+})();
+
+
+(function(){
     //objeto para ver el pisarron
     self.boardView = function(canvas,board){
         this.canvas = canvas;
@@ -30,6 +58,38 @@
         this.board = board;
         this.ctx = canvas.getContext("2d");
     }
+
+    
+
+    self.boardView.prototype= {
+
+        //dibija el pisarron
+    draw: function(){
+        for (var index = this.board.elements.length - 1; index >=0 ; index--) {
+            var el = this.board.elements[index];
+            
+            draw(this.ctx, el);
+        }; 
+
+    }
+
+    }
+    //el meto de dibujado con sus opciones
+    function draw(ctx,element){
+        
+        if(element !== null && element.hasOwnProperty("kind")){
+                switch(element.kind){
+            case "rectangle":
+                ctx.fillRect(element.x,element.y,element.width,element.height);
+                break;
+            
+        }
+        }
+        
+    
+    
+}
+
 })();
 
 window.addEventListener("load",main);
@@ -38,4 +98,7 @@ function main (){
     var board = new Board(800,400);
     var canvas = document.getElementById('canvas');
     var board_view = new boardView(canvas,board);
+    var bar= new Bar(40,150,30,100,board);
+    var bar_2= new Bar(735,150,30,100,board);
+    board_view.draw();
 }
