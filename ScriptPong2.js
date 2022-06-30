ea68061406ccadd2bf000cb841cd8a9de60fb6af
Pablo Acland <pablo.acland@estudiantes.utec.edu.uy>
2022-06-30 20:34:44 -0300

    se realizo lo del video 4

--- ScriptPong2.js
+++ ScriptPong2.js
@@ -22,6 +22,25 @@
 })();
 
 (function(){
+    //objeto pelota
+    self.Ball = function(x,y,radius,board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+
+        this.speed_y = 0;
+        this.speed_x=3;
+        this.speed = 3;
+        this.board = board;
+
+        board.ball = this;
+        this.kind = "circle";
+        
+
+    }
+})();
+
+(function(){
     //objeto Barra
     self.Bar = function(x,y,width,height,board){
         this.x = x;
@@ -67,28 +86,45 @@
 
     self.boardView.prototype= {
 
-        //dibija el pisarron
-    draw: function(){
-        for (var index = this.board.elements.length - 1; index >=0 ; index--) {
-            var el = this.board.elements[index];
-            
-            draw(this.ctx, el);
-        }; 
+        //limpia el pisarron
+        clean: function(){
+            this.ctx.clearRect(0,0,this.board.width,this.board.height);
+        },
 
-    }
+            //dibija el pisarron
+        draw: function(){
+            for (var index = this.board.elements.length - 1; index >=0 ; index--) {
+                var el = this.board.elements[index];
+                
+                draw(this.ctx, el);
+            }; 
+
+        },
+
+        //ejecuta los metodos relacionados con el juego
+        play: function(){
+            this.clean();
+            this.draw();
+        }
 
     }
     //el meto de dibujado con sus opciones
     function draw(ctx,element){
         
-        if(element !== null && element.hasOwnProperty("kind")){
+        
                 switch(element.kind){
             case "rectangle":
                 ctx.fillRect(element.x,element.y,element.width,element.height);
                 break;
-            
+                
+            case "circle":
+                    ctx.beginPath();
+                    ctx.arc(element.x,element.y,element.radius,0,7);
+                    ctx.fill();
+                    ctx.closePath();
+                    break;
         }
-        }
+        
         
     
     
@@ -101,9 +137,11 @@ var board = new Board(800,400);
     var board_view = new boardView(canvas,board);
     var bar= new Bar(40,150,30,100,board);
     var bar_2= new Bar(735,150,30,100,board);
+    var ball = new Ball(400,200,10,board);
 
 //control de inputs del teclado
 document.addEventListener("keydown",function(ev){
+    ev.preventDefault();
     //capta la flecha arriba
     if (ev.keyCode==38) {
         bar.up();
@@ -112,11 +150,20 @@ document.addEventListener("keydown",function(ev){
     else if (ev.keyCode==40) {
         bar.down();
     }
+    //capta la w
+    if (ev.keyCode==87) {
+        bar_2.up();
+    }
+    //capta la s
+    else if (ev.keyCode==83) {
+        bar_2.down();
+    }
 });
 
-window.addEventListener("load",main);
-
+//window.addEventListener("load",main);
+window.requestAnimationFrame(main);
 function main (){
     
-    board_view.draw();
+    board_view.play();
+    window.requestAnimationFrame(main);
 }
